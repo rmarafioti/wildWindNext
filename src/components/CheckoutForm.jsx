deb8052780eed217fa34aaa331cad52d06414ca8
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-07-09 22:07:59 -0500

    implemented stripe payment to app, everything transferred over

---
+++ src/components/CheckoutForm.jsx
@@ -0,0 +1,41 @@
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from "@stripe/react-stripe-js";
+import { useCallback } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe("pk_test_UbdDokx4p8LKUNZRwzbkXZeo00xKo7MiZH");
+
+/**
+ * @component CheckoutForm handles the Stripe api call for user payment processing via the Stripe embedded form
+ */
+export default function CheckoutForm() {
+  const fetchClientSecret = useCallback(() => {
+    // Create a Checkout Session
+    return fetch("/api/payment", {
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => data.clientSecret);
+  }, []);
+
+  const options = { fetchClientSecret };
+
+  return (
+    <div id="checkout">
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout
+          onSuccess={() => {
+            // Handle success here (e.g., redirect to a success page, show a message, etc.)
+            alert("Payment successful!");
+          }}
+          onCancel={() => {
+            // Handle cancel here (e.g., redirect to a cancel page, show a message, etc.)
+            alert("Payment cancelled.");
+          }}
+        />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
+}
