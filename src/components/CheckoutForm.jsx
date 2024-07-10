7c42702713a8c89563c24770946eaa502a839420
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-07-09 22:31:36 -0500

    last check off the night

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -4,6 +4,7 @@ import {
 } from "@stripe/react-stripe-js";
 import { useCallback } from "react";
 import { loadStripe } from "@stripe/stripe-js";
+import { useRouter } from "next/router";
 
 const stripePromise = loadStripe("pk_test_UbdDokx4p8LKUNZRwzbkXZeo00xKo7MiZH");
 
@@ -11,6 +12,7 @@ const stripePromise = loadStripe("pk_test_UbdDokx4p8LKUNZRwzbkXZeo00xKo7MiZH");
  * @component CheckoutForm handles the Stripe api call for user payment processing via the Stripe embedded form
  */
 export default function CheckoutForm() {
+  const router = useRouter();
   const fetchClientSecret = useCallback(() => {
     // Create a Checkout Session
     return fetch("/api/payment", {
@@ -26,9 +28,10 @@ export default function CheckoutForm() {
     <div id="checkout">
       <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
         <EmbeddedCheckout
-          onSuccess={() => {
-            // Handle success here (e.g., redirect to a success page, show a message, etc.)
-            alert("Payment successful!");
+          onSuccess={(event) => {
+            // Handle success here, for example, redirect to the return page
+            const sessionId = event.session_id;
+            router.push(`/return?session_id=${sessionId}`);
           }}
           onCancel={() => {
             // Handle cancel here (e.g., redirect to a cancel page, show a message, etc.)
