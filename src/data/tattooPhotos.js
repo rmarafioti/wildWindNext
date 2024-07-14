f6fcecf15397baba89e4f31af99d9770ade4d8e5
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-07-14 08:10:24 -0500

    reformatted tattoo images wity f_webp for faster load time

--- src/data/tattooPhotos.js
+++ src/data/tattooPhotos.js
@@ -2,102 +2,102 @@ export const richPhotos = [
   {
     id: 1,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295373/wildWindSite/tigerAndLotus_bwkp7d.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295373/wildWindSite/tigerAndLotus_bwkp7d.jpg",
   },
   {
     id: 2,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295387/wildWindSite/nautilus_w9b6k3.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295387/wildWindSite/nautilus_w9b6k3.jpg",
   },
   {
     id: 3,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295387/wildWindSite/memphisStyle_ztf3h7.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295387/wildWindSite/memphisStyle_ztf3h7.jpg",
   },
   {
     id: 4,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295388/wildWindSite/skullFlower_f4nzcs.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295388/wildWindSite/skullFlower_f4nzcs.jpg",
   },
   {
     id: 5,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295400/wildWindSite/noneOfThis_dduo9u.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295400/wildWindSite/noneOfThis_dduo9u.jpg",
   },
   {
     id: 6,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295399/wildWindSite/pantherBackpiece_ret5go.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295399/wildWindSite/pantherBackpiece_ret5go.jpg",
   },
   {
     id: 7,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295399/wildWindSite/eyeBallFlower_abgbe7.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295399/wildWindSite/eyeBallFlower_abgbe7.jpg",
   },
   {
     id: 8,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295399/wildWindSite/picnicAnt_tdxwbk.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295399/wildWindSite/picnicAnt_tdxwbk.jpg",
   },
   {
     id: 9,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295408/wildWindSite/panther_svoaqo.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295408/wildWindSite/panther_svoaqo.jpg",
   },
   {
     id: 10,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295408/wildWindSite/plant_m8dwya.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295408/wildWindSite/plant_m8dwya.jpg",
   },
   {
     id: 11,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295407/wildWindSite/hyenaSleeve_bvkzlo.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295407/wildWindSite/hyenaSleeve_bvkzlo.jpg",
   },
   {
     id: 12,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295407/wildWindSite/gibsonGirl_ma7ldx.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295407/wildWindSite/gibsonGirl_ma7ldx.jpg",
   },
   {
     id: 13,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295420/wildWindSite/reaper_dfj12e.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295420/wildWindSite/reaper_dfj12e.jpg",
   },
   {
     id: 14,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295421/wildWindSite/lionHead_jycw6h.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295421/wildWindSite/lionHead_jycw6h.jpg",
   },
   {
     id: 15,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295419/wildWindSite/diva_j5trva.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295419/wildWindSite/diva_j5trva.jpg",
   },
   {
     id: 16,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295420/wildWindSite/wolf_anrjxn.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1717295420/wildWindSite/wolf_anrjxn.jpg",
   },
   {
     id: 17,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1719753503/wildWindSite/bustBooks_kvyaw7.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1719753503/wildWindSite/bustBooks_kvyaw7.jpg",
   },
   {
     id: 18,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1719753530/wildWindSite/devilBaby_zaxu90.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/q_auto/v1719753530/wildWindSite/devilBaby_zaxu90.jpg",
   },
   {
     id: 19,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1717295419/wildWindSite/hotSauce_ymbvyv.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/f_webp/v1717295419/wildWindSite/hotSauce_ymbvyv.jpg",
   },
   {
     id: 20,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1719753516/wildWindSite/colorBlockFlowers_ejfkdp.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1719753516/wildWindSite/colorBlockFlowers_ejfkdp.jpg",
   },
 ];
 
@@ -105,101 +105,101 @@ export const mercyPhotos = [
   {
     id: 1,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058325/wildWindSite/mercedesTats/mercedesOne_oyn8u0.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058325/wildWindSite/mercedesTats/mercedesOne_oyn8u0.jpg",
   },
   {
     id: 2,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058329/wildWindSite/mercedesTats/mercedesTwo_clbmtt.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058329/wildWindSite/mercedesTats/mercedesTwo_clbmtt.jpg",
   },
   {
     id: 3,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058328/wildWindSite/mercedesTats/mercedesThree_g5cw7v.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058328/wildWindSite/mercedesTats/mercedesThree_g5cw7v.jpg",
   },
   {
     id: 4,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058323/wildWindSite/mercedesTats/mercedesFour_iyuymn.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058323/wildWindSite/mercedesTats/mercedesFour_iyuymn.jpg",
   },
   {
     id: 5,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058323/wildWindSite/mercedesTats/mercedesFive_x4bxe2.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058323/wildWindSite/mercedesTats/mercedesFive_x4bxe2.jpg",
   },
   {
     id: 6,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058326/wildWindSite/mercedesTats/mercedesSix_a3vipa.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058326/wildWindSite/mercedesTats/mercedesSix_a3vipa.jpg",
   },
   {
     id: 7,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058325/wildWindSite/mercedesTats/mercedesSeven_h2ygvd.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058325/wildWindSite/mercedesTats/mercedesSeven_h2ygvd.jpg",
   },
   {
     id: 8,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058322/wildWindSite/mercedesTats/mercedesEight_tka6bo.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058322/wildWindSite/mercedesTats/mercedesEight_tka6bo.jpg",
   },
   {
     id: 9,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058324/wildWindSite/mercedesTats/mercedesNine_suhz1y.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058324/wildWindSite/mercedesTats/mercedesNine_suhz1y.jpg",
   },
   {
     id: 10,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058327/wildWindSite/mercedesTats/mercedesTen_nwvfps.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058327/wildWindSite/mercedesTats/mercedesTen_nwvfps.jpg",
   },
   {
     id: 11,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058322/wildWindSite/mercedesTats/mercedesEleven_yb6dvn.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058322/wildWindSite/mercedesTats/mercedesEleven_yb6dvn.jpg",
   },
   {
     id: 12,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058328/wildWindSite/mercedesTats/mercedesTwelve_dlqann.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058328/wildWindSite/mercedesTats/mercedesTwelve_dlqann.jpg",
   },
   {
     id: 13,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058327/wildWindSite/mercedesTats/mercedesThirteen_sde7ax.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058327/wildWindSite/mercedesTats/mercedesThirteen_sde7ax.jpg",
   },
   {
     id: 14,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058323/wildWindSite/mercedesTats/mercedesFourteen_rkwndl.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058323/wildWindSite/mercedesTats/mercedesFourteen_rkwndl.jpg",
   },
   {
     id: 15,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058323/wildWindSite/mercedesTats/mercedesFifteen_a47pjb.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058323/wildWindSite/mercedesTats/mercedesFifteen_a47pjb.jpg",
   },
   {
     id: 16,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058326/wildWindSite/mercedesTats/mercedesSixteenjpeg_jzewwi.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058326/wildWindSite/mercedesTats/mercedesSixteenjpeg_jzewwi.jpg",
   },
   {
     id: 17,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058325/wildWindSite/mercedesTats/mercedesSeventeen_t06lbp.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058325/wildWindSite/mercedesTats/mercedesSeventeen_t06lbp.jpg",
   },
   {
     id: 18,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058322/wildWindSite/mercedesTats/mercedesEighteen_zqtenc.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058322/wildWindSite/mercedesTats/mercedesEighteen_zqtenc.jpg",
   },
   {
     id: 19,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058325/wildWindSite/mercedesTats/mercedesNineteen_bxqhgy.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058325/wildWindSite/mercedesTats/mercedesNineteen_bxqhgy.jpg",
   },
   {
     id: 20,
     image:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1720058329/wildWindSite/mercedesTats/mercedesTwenty_gtvwji.jpg",
+      "https://res.cloudinary.com/dzpne110u/image/upload/f_webp/v1720058329/wildWindSite/mercedesTats/mercedesTwenty_gtvwji.jpg",
   },
 ];
