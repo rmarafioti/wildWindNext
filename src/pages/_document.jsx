08050be191db4f6717f87aa2db63c50b10e60818
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-07-24 16:46:50 -0500

    added favicon

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" type="image/x-icon" href="/wildWindFavicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
