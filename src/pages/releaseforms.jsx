92d8278cadd420c4066de49943067f1cde0b72bf
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-08-13 13:01:29 -0500

    finished implementing release form functionality for Rich's tats

--- src/pages/releaseforms.jsx
+++ src/pages/releaseforms.jsx
@@ -25,18 +25,14 @@ export default function Releaseforms() {
         Please choose your artist to fill out their waiver, release & consent to
         tattoo:
       </h2>
-      <ul className={styles.artistList}>
-        <li className={styles.artistLink}>Rich Marafioti</li>
-        <li className={styles.artistLink}>Mercy Wright</li>
-      </ul>
-      {/*<iframe
-        src="https://na4.docusign.net/Member/PowerFormSigning.aspx?PowerFormId=2db995c9-063e-47bf-8437-6b7fcfcfa9a3&env=na4&acct=b4ee9f55-3f08-40c4-88c9-4b9a5aa9e820&v=2"
-        width="100%"
-        height="1000px"
-        frameborder="0"
-      >
-        Your browser does not support iframes.
-      </iframe>*/}
+      <div className={styles.artistList}>
+        <a
+          className={styles.artistLink}
+          href="https://na4.docusign.net/Member/PowerFormSigning.aspx?PowerFormId=2db995c9-063e-47bf-8437-6b7fcfcfa9a3&env=na4&acct=b4ee9f55-3f08-40c4-88c9-4b9a5aa9e820&v=2"
+        >
+          Rich Marafioti
+        </a>
+      </div>
     </article>
   );
 }
