042af10ca7f135af195498161a63f7ba2948f51b
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-08-13 11:43:35 -0500

    created release form page and linked to embedded Docusign form

---
+++ src/pages/releaseforms.jsx
@@ -0,0 +1,20 @@
+export default function Releaseforms() {
+  return (
+    <>
+      <h1>Release Forms</h1>
+      <h2>Please choose the artist you are getting tattooed by:</h2>
+      <ul>
+        <li>Rich Marafioti</li>
+        <li>Mercy Wright</li>
+      </ul>
+      {/*<iframe
+        src="https://na4.docusign.net/Member/PowerFormSigning.aspx?PowerFormId=2db995c9-063e-47bf-8437-6b7fcfcfa9a3&env=na4&acct=b4ee9f55-3f08-40c4-88c9-4b9a5aa9e820&v=2"
+        width="100%"
+        height="1000px"
+        frameborder="0"
+      >
+        Your browser does not support iframes.
+      </iframe>*/}
+    </>
+  );
+}
