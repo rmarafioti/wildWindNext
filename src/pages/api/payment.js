f0d86b5c4ca1e127659e629726cbee59cf9d522a
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-09-04 21:08:49 -0500

    final commit before push

--- src/pages/api/payment.js
+++ src/pages/api/payment.js
@@ -1,3 +1,39 @@
+/*import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-11-15",
+});
+
+export default async function handler(
+  req = NextApiRequest,
+  res = NextApiResponse
+) {
+  if (req.method === "POST") {
+    try {
+      const session = await stripe.checkout.sessions.create({
+        mode: "payment",
+        payment_method_types: ["card"],
+        line_items: [
+          {
+            price: "price_1PLr75Clew7ziQf5NnzXpF1B",
+            quantity: 1,
+          },
+        ],
+        ui_mode: "embedded",
+        redirect_on_completion: "never",
+      });
+
+      res.status(200).json({ clientSecret: session.client_secret });
+    } catch (error) {
+      console.error("Error creating Stripe checkout session:", error);
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}*/
+
 import Stripe from "stripe";
 import { buffer } from "micro"; // Import buffer for raw body parsing (or use Node.js buffer alternative)
 
