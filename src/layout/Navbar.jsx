18c22914a5b3f7ed8fdcd5858dbdf44f15d1c5cd
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-09-07 00:48:08 -0500

    saving progress for the night

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState, useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { IoIosArrowDown } from "react-icons/io";
-import { IoCloseOutline } from "react-icons/io5";
-import { IoIosArrowForward } from "react-icons/io";
+import {
+  IoIosArrowDown,
+  IoCloseOutline,
+  IoIosArrowForward,
+} from "react-icons/io";
 import useDropdown from "../components/dropDown";
 
 import styles from "./Navbar.module.css";
@@ -14,27 +16,18 @@ import styles from "./Navbar.module.css";
  */
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
-  const aboutDropdown = useDropdown();
-  const tattooDropdown = useDropdown();
   const location = useRouter();
 
-  const aboutDropdownRef = useRef(aboutDropdown);
-  const tattooDropdownRef = useRef(tattooDropdown);
-
-  //reset menu and dropdown to closed on state change
+  // Reset menu and dropdowns on location (route) change
   useEffect(() => {
     setMenuOpen(false);
-    aboutDropdownRef.current.closeDropdown();
-    tattooDropdownRef.current.closeDropdown();
   }, [location.pathname]);
 
   /**
-   * @toggleMenu open and close hamburger menu && close drop down menu
+   * @toggleMenu open and close hamburger menu && close dropdown menus
    */
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
-    aboutDropdownRef.current.closeDropdown();
-    tattooDropdownRef.current.closeDropdown();
   };
 
   return (
@@ -117,13 +110,6 @@ export default function Navbar() {
           </ul>
         </li>
         <li className={styles.navItem}>
-          <Link className={styles.navContainer} href="/contact">
-            <h4 className={styles.link} role="navbar element Contact">
-              CONTACT
-            </h4>
-          </Link>
-        </li>
-        <li className={styles.navItem}>
           <Link className={styles.navContainer} href="/aftercare">
             <h4 className={styles.link} role="navbar element Aftercare">
               AFTERCARE
@@ -132,7 +118,6 @@ export default function Navbar() {
         </li>
         <li className={styles.navItem}>
           <Link className={styles.navContainer} href="/faqs">
-            {/*<HiOutlineEnvelope className={styles.linkIcon} />*/}
             <h4 className={styles.link} role="navbar element Contact">
               FAQs
             </h4>
@@ -140,129 +125,76 @@ export default function Navbar() {
         </li>
         <li className={styles.navItem}>
           <Link className={styles.navContainer} href="/giftcards">
-            {/*<IoGiftOutline className={styles.linkIcon} />*/}
             <h4
               className={styles.link}
               id={styles.giftCards}
-              role="navbar element Giftcars"
+              role="navbar element Giftcards"
             >
               GIFTCARDS
             </h4>
           </Link>
         </li>
       </menu>
-      {/*drop down menu open*/}
-      <div id={styles.dropDownContainer}>
-        <menu
-          className={`${styles.drop} ${
-            aboutDropdown.isOpen ? styles.active : ""
-          }`}
-        >
-          <Link
-            className={styles.dropLink}
-            id={styles.dropLinkTop}
-            href="/reviews"
-          >
-            <h4 className={styles.dropLinkName}>Reviews</h4>
-            <IoIosArrowForward />
-          </Link>
-          <Link
-            className={styles.dropLink}
-            id={styles.dropLinkMiddle}
-            href="/media"
-          >
-            <h4 className={styles.dropLinkName}>Media</h4>
-            <IoIosArrowForward />
-          </Link>
-          <Link className={styles.dropLink} href="/faqs">
-            <h4 className={styles.dropLinkName}>FAQs</h4>
-            <IoIosArrowForward />
-          </Link>
-        </menu>
-      </div>
 
       {/* mobile menu */}
       <menu className={`${styles.menu} ${menuOpen ? styles.active : ""}`}>
-        <li id={styles.aboutItem}>
-          <Link className={styles.navContainer} href="/aboutus">
-            {/*<GoBook className={styles.linkIcon} />*/}
-            <h4
-              className={styles.link}
-              id={styles.aboutUs}
-              role="navbar element About Us"
-            >
-              ABOUT US
-            </h4>
-          </Link>
-          {/*drop down menu togglebutton*/}
-          <div
-            className={styles.aboutLink}
-            id={styles.dropIcon}
-            onClick={aboutDropdown.toggleDropdown}
-          >
-            {aboutDropdown.isOpen ? <IoCloseOutline /> : <IoIosArrowDown />}
-          </div>
-        </li>
-        <li id={styles.tattooItem}>
-          <Link className={styles.navContainer} href="/tattoos">
-            {/*<IoSkullOutline className={styles.linkIcon} />*/}
-            <h4
-              className={styles.link}
-              id={styles.tats}
-              role="navbar element Tattoos"
-            >
-              TATTOOS
-            </h4>
-          </Link>
-          {/*drop down menu togglebutton*/}
-          <div
-            className={styles.aboutLink}
-            id={styles.dropIcon}
-            onClick={tattooDropdown.toggleDropdown}
-          >
-            {tattooDropdown.isOpen ? <IoCloseOutline /> : <IoIosArrowDown />}
-          </div>
-          {/*drop down menu open*/}
-          <div id={styles.dropDownContainer}>
-            <menu
-              className={`${styles.droptats} ${
-                tattooDropdown.isOpen ? styles.active : ""
-              }`}
-            >
-              <Link
-                className={styles.dropLink}
-                id={styles.dropLinkTop}
-                href="/richtats"
-              >
-                <h4 className={styles.dropLinkName}>Rich Marafioti</h4>
-                <IoIosArrowForward />
-              </Link>
-              <Link className={styles.dropLink} href="/mercytats">
-                <h4 className={styles.dropLinkName}>Mercy Wright</h4>
-                <IoIosArrowForward />
-              </Link>
-              <Link className={styles.dropLink} href="/trevortats">
-                <h4 className={styles.dropLinkName}>Trevor Aarsvold</h4>
-                <IoIosArrowForward />
-              </Link>
-              <Link className={styles.dropLink} href="/allietats">
-                <h4 className={styles.dropLinkName}>Allie Sider</h4>
-                <IoIosArrowForward />
-              </Link>
-            </menu>
-          </div>
-        </li>
         <li className={styles.navItem}>
           <Link className={styles.navContainer} href="/contact">
-            {/*<HiOutlineEnvelope className={styles.linkIcon} />*/}
             <h4 className={styles.link} role="navbar element Contact">
-              CONTACT
+              CONTACT US
             </h4>
           </Link>
+        </li>
+        <li id={styles.tattooItem}>
+          <section>
+            <Link className={styles.navContainer} href="/tattoos">
+              <h4
+                className={styles.link}
+                id={styles.tats}
+                role="navbar element Tattoos"
+              >
+                TATTOOS
+              </h4>
+            </Link>
+            <ul className={styles.subCategory}>
+              <Link className={styles.subLink} href="/richtats">
+                <li className={styles.subItem}>Rich Marafioti</li>
+              </Link>
+              <Link className={styles.subLink} href="/mercytats">
+                <li className={styles.subItem}>Mercy Wright</li>
+              </Link>
+              <Link className={styles.subLink} href="/trevortats">
+                <li className={styles.subItem}>Trevor Aarsvold</li>
+              </Link>
+              <Link className={styles.subLink} href="/allietats">
+                <li className={styles.subItem}>Allie Sider</li>
+              </Link>
+            </ul>
+          </section>
+        </li>
+        <li id={styles.aboutItem}>
+          <section>
+            <Link className={styles.navContainer} href="/aboutus">
+              <h4
+                className={styles.link}
+                id={styles.aboutUs}
+                role="navbar element About Us"
+              >
+                ABOUT US
+              </h4>
+            </Link>
+            <ul className={styles.subCategory}>
+              <Link className={styles.subLink} href="/reviews">
+                <li className={styles.subItem}>Reviews</li>
+              </Link>
+              <Link className={styles.subLink} href="/media">
+                <li className={styles.subItem}>Media</li>
+              </Link>
+            </ul>
+          </section>
         </li>
         <li className={styles.navItem}>
           <Link className={styles.navContainer} href="/aftercare">
-            {/*<IoHeartOutline className={styles.linkIcon} />*/}
             <h4 className={styles.link} role="navbar element Aftercare">
               AFTERCARE
             </h4>
@@ -270,7 +202,6 @@ export default function Navbar() {
         </li>
         <li className={styles.navItem}>
           <Link className={styles.navContainer} href="/faqs">
-            {/*<HiOutlineEnvelope className={styles.linkIcon} />*/}
             <h4 className={styles.link} role="navbar element Contact">
               FAQs
             </h4>
@@ -278,46 +209,16 @@ export default function Navbar() {
         </li>
         <li className={styles.navItem}>
           <Link className={styles.navContainer} href="/giftcards">
-            {/*<IoGiftOutline className={styles.linkIcon} />*/}
             <h4
               className={styles.link}
               id={styles.giftCards}
-              role="navbar element Giftcars"
+              role="navbar element Giftcards"
             >
               GIFTCARDS
             </h4>
           </Link>
         </li>
       </menu>
-      {/*drop down menu open*/}
-      <div id={styles.dropDownContainer}>
-        <menu
-          className={`${styles.drop} ${
-            aboutDropdown.isOpen ? styles.active : ""
-          }`}
-        >
-          <Link
-            className={styles.dropLink}
-            id={styles.dropLinkTop}
-            href="/reviews"
-          >
-            <h4 className={styles.dropLinkName}>Reviews</h4>
-            <IoIosArrowForward />
-          </Link>
-          <Link
-            className={styles.dropLink}
-            id={styles.dropLinkMiddle}
-            href="/media"
-          >
-            <h4 className={styles.dropLinkName}>Media</h4>
-            <IoIosArrowForward />
-          </Link>
-          <Link className={styles.dropLink} href="/faqs">
-            <h4 className={styles.dropLinkName}>FAQs</h4>
-            <IoIosArrowForward />
-          </Link>
-        </menu>
-      </div>
     </>
   );
 }
